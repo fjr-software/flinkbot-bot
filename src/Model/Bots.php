417cdf9232a5e224c74d0f56078ceaddd34cdb8e
Francisco Jr <franciscojr.dev@gmail.com>
2023-12-25 18:04:54 -0300

    Changes

--- src/Model/Bots.php
+++ src/Model/Bots.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace FjrSoftware\Flinkbot\Bot\Model;
 
+use FjrSoftware\Flinkbot\Bot\Security;
 use Illuminate\Database\Eloquent\Model;
 
 class Bots extends Model
@@ -14,4 +15,24 @@ class Bots extends Model
      * @var string
      */
     protected $table = 'bots';
+
+    /**
+     * Get api key
+     *
+     * @return string
+     */
+    public function getApiKey(): string
+    {
+        return Security::decrypt($this->api_key);
+    }
+
+    /**
+     * Get api secret
+     *
+     * @return string
+     */
+    public function getApiSecret(): string
+    {
+        return Security::decrypt($this->api_secret);
+    }
 }

f9097c79ca278bfd7e571af928246664c62a3684
Francisco Jr <franciscojr.dev@gmail.com>
2023-12-25 18:37:45 -0300

    Changes

---
+++ src/Model/Orders.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FjrSoftware\Flinkbot\Bot\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    /**
+     * The table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'orders';
+}

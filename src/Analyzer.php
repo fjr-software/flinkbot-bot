673804d8ffbefb3a8d35e3c41540faef620e8008
Francisco Jr <franciscojr.dev@gmail.com>
2023-12-29 08:07:23 -0300

    Changes

--- src/Analyzer.php
+++ src/Analyzer.php
@@ -242,8 +242,8 @@ class Analyzer
 
                         if (!$openOrdersClosed) {
                             if ($canPrevent) {
-                                $diffPrice = $this->bot->getExchange()->calculeProfit($markPrice, (float) ($configPosition['profit'] / $position->leverage));
-                                $priceCloseGain = (float) ($position->side === 'SHORT' ? $markPrice - $diffPrice : $markPrice + $diffPrice);
+                                $diffPrice = $this->bot->getExchange()->calculeProfit($entryPrice, (float) ($configPosition['profit'] / $position->leverage));
+                                $priceCloseGain = (float) ($position->side === 'SHORT' ? $entryPrice - $diffPrice : $entryPrice + $diffPrice);
                                 $priceCloseGain = $this->bot->getExchange()->formatDecimal($markPrice, $priceCloseGain);
                                 $typeClosed = 'prevent';
                             }

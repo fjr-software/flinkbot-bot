6f0257f0d7aeaa416c1d67ea7b2786631c929b92
Francisco Jr <franciscojr.dev@gmail.com>
2023-12-30 11:32:59 -0300

    Changes

---
+++ src/Model/ApiRateLimit.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FjrSoftware\Flinkbot\Bot\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ApiRateLimit extends Model
+{
+    /**
+     * The table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'api_rate_limit';
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'request_ip',
+        'country',
+        'exchange',
+        'request_count',
+        'request_last_time',
+        'request_rate_limit',
+        'request_reset_interval',
+        'order_count',
+        'order_last_time',
+        'order_rate_limit',
+        'order_reset_interval',
+        'status',
+    ];
+}

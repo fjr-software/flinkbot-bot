6f29ca9f7511d35861e169596f0e3c871e12402e
Francisco Jr <franciscojr.dev@gmail.com>
2024-01-01 22:44:55 -0300

    Changes

--- src/Analyzer.php
+++ src/Analyzer.php
@@ -481,18 +481,18 @@ class Analyzer
                         'status' => 'active'
                     ])->first();
 
-                    $checkSideBot = strtoupper($this->bot->getConfig()->getOperationSide()) !== 'BOTH';
-                    $checkSideSymbol = $symbolConfig->side !== 'BOTH';
+                    $checkSideBot = strtoupper($this->bot->getConfig()->getOperationSide()) != 'BOTH';
+                    $checkSideSymbol = $symbolConfig->side != 'BOTH';
                     $sideChecked = '';
                     $validSideBot = true;
                     $validSideSymbol = true;
 
                     if ($checkSideBot) {
-                        $validSideBot = $side !== strtoupper($this->bot->getConfig()->getOperationSide());
+                        $validSideBot = $side != strtoupper($this->bot->getConfig()->getOperationSide());
                     }
 
                     if ($checkSideSymbol) {
-                        $validSideSymbol = $side !== $symbolConfig->side;
+                        $validSideSymbol = $side != $symbolConfig->side;
                     }
 
                     if (!$validSideBot) {

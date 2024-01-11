97aa8d233a38686d114cba2852c9cca5f21ce2f6
Francisco Jr <franciscojr.dev@gmail.com>
2024-01-11 14:49:03 -0300

    Changes

---
+++ src/Model/AccountValueCycle.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace FjrSoftware\Flinkbot\Bot\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AccountValueCycle extends Model
+{
+    /**
+     * The table associated with the model
+     *
+     * @var string
+     */
+    protected $table = 'account_value_cycle';
+
+    /**
+     * The attributes that are mass assignable
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'bot_id',
+        'period',
+        'current_value',
+        'target_value',
+        'done',
+    ];
+}
